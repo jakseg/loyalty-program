3e818dce96200b0daa38f4cbe358078c0baaac6d
Ehsan <80709394+Esafi2005@users.noreply.github.com>
2025-06-24 10:47:46 +0200

    frontend code added

---
+++ contracts/LoyaltyProgram.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract LoyaltyProgram {
+    function greet() public pure returns (string memory) {
+        return "Hello from LoyaltyProgram!";
+    }
+}
